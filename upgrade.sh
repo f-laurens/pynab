51dcd5ee65230e6d1b0f4dd3c57c5bf1e5ce4216
Fabrice Laurens <flaurens@cs.stanford.edu>
2022-04-19 17:59:56 +0200

    setup: Pynab (and drivers) now installed in /opt directory.

--- upgrade.sh
+++ upgrade.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 
 root_dir=`sed -nE -e 's|WorkingDirectory=(.+)|\1|p' < /lib/systemd/system/nabd.service`
 owner=`stat -c '%U' ${root_dir}`
-ownerid=`stat -c '%u' ${root_dir}`
+uid=`stat -c '%u' ${root_dir}`
 
 step="init"
 if [ "${1:-}" == "install" ]; then
@@ -21,18 +21,18 @@ case $step in
     for service_file in */*.service ; do
       name=`basename ${service_file}`
       if [ "${name}" != "nabd.service" -a "${name}" != "nabweb.service" ]; then
-        sudo systemctl stop ${name} || echo -n ""
+        sudo systemctl stop ${name} || true
       fi
     done
-    sudo systemctl stop nabd.socket || echo -n ""
-    sudo systemctl stop nabd.service || echo -n ""
+    sudo systemctl stop nabd.socket || true
+    sudo systemctl stop nabd.service || true
   
     echo "Updating Pynab"
     sudo -u ${owner} touch /tmp/pynab.upgrade
     sudo chown ${owner} /tmp/pynab.upgrade
     echo "Updating Pynab - 1/?" > /tmp/pynab.upgrade
     cd ${root_dir}
-    if [[ $EUID -ne ${ownerid} ]]; then
+    if [[ $EUID -ne ${uid} ]]; then
       sudo -u ${owner} git pull
     else
       git pull
